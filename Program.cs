974e027e75270578b7b5d5186be18d01a54c48ee
netonblpb <skullbog@gmail.com>
2022-02-11 13:22:09 +0300

    Task24 complete

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+
+for (int i = 1; i <= num; i++)
+{    
+    Console.Write($"{i} в третьей степени равно {Math.Pow(i, 3)}");
+    Console.WriteLine();        
+}
